fc3fc0e1cf2b45604be60a46cb07687628a21481
kllmctrl <kllmctrl@gmail.com>
2017-03-19 12:12:42 +0800

    replace project

---
+++ Assets/Scripts/UIShoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIShoot : MonoBehaviour {
+	public Texture2D shotCenter;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
