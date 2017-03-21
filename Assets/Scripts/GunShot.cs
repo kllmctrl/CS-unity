01febc1752438b3a667840799b7128fb9e0ec7bb
kllmctrl <kllmctrl@gmail.com>
2017-03-22 05:54:50 +0800

    add gun

---
+++ Assets/Scripts/GunShot.cs
@@ -0,0 +1,22 @@
+﻿//***************************************************************
+///   @ Brief:   射击逻辑
+///   @ Author:  kc
+///   @ Date:    2017-3-19
+//***************************************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShot : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/UIShoot.cs
+++ Assets/Scripts/UIShoot.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+﻿//***************************************************************
+///   @ Brief:   显示阻击焦点
+///   @ Author:  kc
+///   @ Date:    2017-3-19
+//***************************************************************
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class UIShoot : MonoBehaviour {
 	public Texture2D shotCenter;
-
+	Rect rectShotCenter;
 
 	// Use this for initialization
 	void Start () {
-		
+		float centerSize = Screen.height * 0.05f;
+		rectShotCenter = new Rect ((Screen.width - centerSize) * 0.5f, (Screen.height - centerSize) * 0.5f, centerSize, centerSize);
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frames
 	void Update () {
 		
+	}
+
+	void OnGUI(){
+		GUI.DrawTexture (rectShotCenter, shotCenter);
 	}
 }
