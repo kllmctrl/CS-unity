eeca06806e826e3cd604f366ead36540cde95953
kllmctrl <kllmctrl@gmail.com>
2017-03-24 07:02:17 +0800

    add bulletHenJi

---
+++ Assets/Scripts/BulletHenJiCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHenJiCtrl : MonoBehaviour {
+	float birthTime = 0;
+	public float keepTime = 5;
+
+	// Use this for initialization
+	void Start () {
+		birthTime = Time.time;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((Time.time - birthTime) > keepTime){
+			Destroy (gameObject);
+		}
+	}
+}
--- Assets/Scripts/GunShot.cs
+++ Assets/Scripts/GunShot.cs
@@ -9,14 +9,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GunShot : MonoBehaviour {
-	
+
+	AudioSource ShootSound;
+	public GameObject bulletHenJi;
+
 	// Use this for initialization
 	void Start () {
-		
+		ShootSound = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if(Input.GetMouseButtonDown(0)){
+			ShootSound.Play ();
+
+			Ray ray = Camera.main.ScreenPointToRay (new Vector2(Screen.width * 0.5f, Screen.height * 0.5f));
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)){
+				//Debug.Log ("####  hit name  ####" + hit.transform.name);
+				//clone Henji
+				GameObject henJi = (GameObject)Instantiate(bulletHenJi);
+				henJi.transform.position = hit.point;
+				henJi.transform.LookAt (hit.point + hit.normal);
+			}
+
+			
+		}
+
 	}
 }
